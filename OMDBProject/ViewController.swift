4486d37e54be6001d489f5551fa7ade8331012b7
Prabhu Beeman <prabhu.beeman@gmail.com>
2018-07-08 22:39:27 +0100

    Initial setup

---
+++ OMDBProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OMDBProject
+//
+//  Created by Prabhu Beeman on 08/07/2018.
+//  Copyright © 2018 The Movie Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
